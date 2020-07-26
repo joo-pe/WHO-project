d7eebde75e153477397157c271bb221e5d388c0a
joo-pe <joohyung1002@naver.com>
2020-07-26 11:55:41 +0900

    init push

--- src/main/java/com/who/WhoApplication.java
+++ src/main/java/com/who/WhoApplication.java
@@ -3,6 +3,9 @@ package com.who;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * created by jaden
+ */
 @SpringBootApplication
 public class WhoApplication {
     public static void main(String[] args) {
