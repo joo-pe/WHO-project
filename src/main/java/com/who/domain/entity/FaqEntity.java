f1af4f48d8f3d40540eb51b1a4b42723c4fe6247
ellendusrud <31755208+ellendusrud@users.noreply.github.com>
2020-09-15 16:56:04 +0900

    메인 완성 faq 완성 로그인 오류...

--- src/main/java/com/who/domain/entity/FaqEntity.java
+++ src/main/java/com/who/domain/entity/FaqEntity.java
@@ -5,9 +5,6 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 
